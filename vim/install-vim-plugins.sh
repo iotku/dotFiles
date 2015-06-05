7298b2e81c16893c82d4facb1682d9d141459966
Jonathan A <me@iotku.pw>
2015-06-04 18:04:15 -0700

    [VIM] Add airline statusbar plugin

--- vim/install-vim-plugins.sh
+++ vim/install-vim-plugins.sh
@@ -5,4 +5,5 @@ if [ ! -f ${HOME}/.vim/autoload/pathogen.vim ]; then
 fi
 cp "./vimrc" "${HOME}/.vimrc"
 git clone https://github.com/trusktr/seti.vim ${HOME}/.vim/bundle/seti.vim
+git clone https://github.com/bling/vim-airline.git ${HOME}/.vim/bundle/vim-airline
 git clone https://github.com/terryma/vim-multiple-cursors.git ${HOME}/.vim/bundle/vim-multiple-cursors
