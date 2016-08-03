8a813a99593304ffd38d90ad0fd71b4ee7f225ce
Jonathan A <iotku@iotku.pw>
2016-08-02 17:23:41 -0700

    [VIM] Change colorscheme to Hybrid

--- vim/install-vim-plugins.sh
+++ vim/install-vim-plugins.sh
@@ -5,6 +5,6 @@ if [ ! -f ${HOME}/.vim/autoload/pathogen.vim ]; then
 fi
 cp "./vimrc" "${HOME}/.vimrc"
 git clone --depth=1 https://github.com/rust-lang/rust.vim.git ${HOME}/.vim/bundle/rust.vim
-git clone --depth=1 https://github.com/trusktr/seti.vim ${HOME}/.vim/bundle/seti.vim
 git clone --depth=1 https://github.com/bling/vim-airline.git ${HOME}/.vim/bundle/vim-airline
+git clone --depth=1 https://github.com/scwood/vim-hybrid.git ${HOME}/.vim/bundle/vim-hybrid
 git clone --depth=1 https://github.com/terryma/vim-multiple-cursors.git ${HOME}/.vim/bundle/vim-multiple-cursors
