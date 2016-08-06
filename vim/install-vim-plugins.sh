52779829bd751fbf2805dca8592b0213c166ce4e
Jonathan A <iotku@iotku.pw>
2016-08-06 16:32:31 -0700

    Switch colorscheme to jellybeans || default syntax on

--- vim/install-vim-plugins.sh
+++ vim/install-vim-plugins.sh
@@ -4,7 +4,7 @@ if [ ! -f ${HOME}/.vim/autoload/pathogen.vim ]; then
    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim 
 fi
 cp "./vimrc" "${HOME}/.vimrc"
+git clone --depth=1 https://github.com/nanotech/jellybeans.vim.git ${HOME}/.vim/bundle/jellybeans.vim
 git clone --depth=1 https://github.com/rust-lang/rust.vim.git ${HOME}/.vim/bundle/rust.vim
 git clone --depth=1 https://github.com/bling/vim-airline.git ${HOME}/.vim/bundle/vim-airline
-git clone --depth=1 https://github.com/scwood/vim-hybrid.git ${HOME}/.vim/bundle/vim-hybrid
 git clone --depth=1 https://github.com/terryma/vim-multiple-cursors.git ${HOME}/.vim/bundle/vim-multiple-cursors
