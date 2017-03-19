916d08ff977af9f59bc67965a67716160c469ee6
Jonathan A <iotku@iotku.pw>
2017-03-19 02:06:45 -0700

    Update dotfiles...

--- gather-dotfiles.sh
+++ gather-dotfiles.sh
@@ -9,11 +9,11 @@ echo '   curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim ' >
 echo 'fi' >> $VIMPLUGINS 
 echo 'cp "./vimrc" "${HOME}/.vimrc"' >> $VIMPLUGINS
 for dir in ${HOME}/.vim/bundle/* ; do
-        GITURL=$(grep url "$dir/.git/config" | cut -b 8-)
+        GITURL=$(grep url "$dir/.git/config" | cut -b 8- | head -n1)
         GITNAME=$(basename $dir)
         DIRNAME='${HOME}/.vim/bundle/'
 
-        echo "git clone --depth=1 ${GITURL} ${DIRNAME}${GITNAME}" >> $VIMPLUGINS
+        echo "git clone --recursive --depth=1 ${GITURL} ${DIRNAME}${GITNAME}" >> $VIMPLUGINS
 done
 chmod +x "$VIMPLUGINS"
 
--- vim/install-vim-plugins.sh
+++ vim/install-vim-plugins.sh
@@ -4,7 +4,10 @@ if [ ! -f ${HOME}/.vim/autoload/pathogen.vim ]; then
    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim 
 fi
 cp "./vimrc" "${HOME}/.vimrc"
-git clone --depth=1 https://github.com/nanotech/jellybeans.vim.git ${HOME}/.vim/bundle/jellybeans.vim
-git clone --depth=1 https://github.com/rust-lang/rust.vim.git ${HOME}/.vim/bundle/rust.vim
-git clone --depth=1 https://github.com/bling/vim-airline.git ${HOME}/.vim/bundle/vim-airline
-git clone --depth=1 https://github.com/terryma/vim-multiple-cursors.git ${HOME}/.vim/bundle/vim-multiple-cursors
+git clone --recursive --depth=1 https://github.com/w0rp/ale.git ${HOME}/.vim/bundle/ale
+git clone --recursive --depth=1 https://github.com/davidhalter/jedi-vim.git ${HOME}/.vim/bundle/jedi-vim
+git clone --recursive --depth=1 https://github.com/nanotech/jellybeans.vim.git ${HOME}/.vim/bundle/jellybeans.vim
+git clone --recursive --depth=1 https://github.com/rust-lang/rust.vim.git ${HOME}/.vim/bundle/rust.vim
+git clone --recursive --depth=1 https://github.com/ervandew/supertab.git ${HOME}/.vim/bundle/supertab
+git clone --recursive --depth=1 https://github.com/bling/vim-airline.git ${HOME}/.vim/bundle/vim-airline
+git clone --recursive --depth=1 https://github.com/terryma/vim-multiple-cursors.git ${HOME}/.vim/bundle/vim-multiple-cursors
