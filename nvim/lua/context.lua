2c309e70228a56eb1c23d2d57f84443153da2d92
Jonathan A <me@iotku.pw>
2022-04-10 21:08:16 -0700

    NVIM: Change treesitter ensure_installed to all, as maintained is depreciated

--- nvim/lua/context.lua
+++ nvim/lua/context.lua
@@ -11,7 +11,7 @@ require("nvim-gps").setup()
 require("colorizer").setup()
 -- treesitter
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = "all", -- "all" or a list of languages
   ignore_install = {}, -- List of parsers to ignore installing
   highlight = {
     enable = true,              -- false will disable the whole extension
