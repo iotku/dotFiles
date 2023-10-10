da7fb1d73c62aabf7dbb94a61191860136fb6d2b
Jonathan A. <me@iotku.pw>
2023-10-10 10:54:01 -0700

    [NVIM] Migrate from packer to lazy.nvim (needs further efforts to make sure things load properly)

--- nvim/lua/packages.lua
+++ nvim/lua/packages.lua
@@ -1,72 +1,78 @@
-local fn = vim.fn
--- Bootstrap Packer (Package Management) -- Remember to :PackerInstall
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-  vim.cmd 'packadd packer.nvim'
+--local fn = vim.fn --needed still after upgrading from packer?
+
+-- bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
--- Packages
-require('packer').startup(function()
-    use 'wbthomason/packer.nvim' -- Packception
+require("lazy").setup({
+    "folke/which-key.nvim",
+    { "folke/neoconf.nvim", cmd = "Neoconf" },
+
     -- Appearance/Bars
-    use 'folke/tokyonight.nvim'
-    use 'xiyaowong/nvim-transparent'
-    use 'lukas-reineke/indent-blankline.nvim'
-    use 'windwp/nvim-autopairs'
-    use 'nvim-tree/nvim-tree.lua'
-    use 'nvim-tree/nvim-web-devicons'
-    use {'nvim-lualine/lualine.nvim', requires = { 'nvim-tree/nvim-web-devicons', opt = true }}
-    use 'norcalli/nvim-colorizer.lua'
-    use 'ntpeters/vim-better-whitespace'
-    use {"folke/todo-comments.nvim", requires = "nvim-lua/plenary.nvim", config = function()
-        require("todo-comments").setup {} end}
-    use { "folke/which-key.nvim", config = function() require("which-key").setup{} end }
-    use 'kosayoda/nvim-lightbulb'
-    use {'j-hui/fidget.nvim', tag='legacy', config = function() require("fidget").setup{} end}
-    use { 'gelguy/wilder.nvim', config = function()  end, }
-    -- Treesitter
-    use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
-    use {'lewis6991/spellsitter.nvim',  config = function() require('spellsitter').setup() end}
-    -- Completion
-    use 'hrsh7th/nvim-cmp'
-    use 'hrsh7th/cmp-nvim-lua'
-    use 'hrsh7th/cmp-buffer'
-    use 'hrsh7th/cmp-path'
-    use 'hrsh7th/cmp-nvim-lsp'     -- LSP source for nvim-cmp
-    use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
-    use 'L3MON4D3/LuaSnip'         -- Snippets plugin
-    -- Language/LSP Support
-    use 'neovim/nvim-lspconfig'
-    use 'ray-x/lsp_signature.nvim'
-    use 'nvim-lua/lsp-status.nvim'
-    use 'ray-x/go.nvim'
-    use 'simrat39/rust-tools.nvim'
-    use 'mfussenegger/nvim-jdtls'
-    use {"SmiteshP/nvim-navic", requires = "neovim/nvim-lspconfig"}
-    -- Notetaking / Wiki
- --   use 'lervag/vimtex'
-    --use 'vimwiki/vimwiki'
-    -- Git
- --   use 'tpope/vim-fugitive'
- --   use 'tpope/vim-rhubarb'
-    use 'junegunn/gv.vim'
-    use {'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' }}
-    -- DAP (Debug Adapter Protocol)
-    use 'mfussenegger/nvim-dap'
-    use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} }
-    use 'theHamsta/nvim-dap-virtual-text'
-    use 'leoluz/nvim-dap-go'
-    -- Fuzzy Finding
-    use 'nvim-lua/plenary.nvim' -- required by telescope
-    use 'nvim-telescope/telescope.nvim'
-    use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make'}
-    use {'nvim-telescope/telescope-ui-select.nvim' }
-    use {"folke/trouble.nvim", requires = "kyazdani42/nvim-web-devicons",
-        config = function()
-            require("trouble").setup {}
-        end
-    }
-    -- use 'github/copilot.vim'   -- nice meme
-end)
+    "folke/tokyonight.nvim",
+    -- "xiyaowong/nvim-transparent",
+    "ntpeters/vim-better-whitespace",
+    "lukas-reineke/indent-blankline.nvim",
+    "windwp/nvim-autopairs",
+    "nvim-tree/nvim-tree.lua",
+    "nvim-tree/nvim-web-devicons",
+    "nvim-lualine/lualine.nvim",
+    "gelguy/wilder.nvim", -- Does this actually init correctly without the config block?
 
+    -- Completion
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-nvim-lua",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-nvim-lsp",     -- LSP source for nvim-cm
+    "saadparwaiz1/cmp_luasnip", -- Snippets source for nvim-cmp
+    "L3MON4D3/LuaSnip",         -- Snippets plugin
+
+    -- Language/LSP Support
+    "neovim/nvim-lspconfig",
+    "ray-x/lsp_signature.nvim",
+    "nvim-lua/lsp-status.nvim",
+    "ray-x/go.nvim",
+    "simrat39/rust-tools.nvim",
+    "mfussenegger/nvim-jdtls",
+    "SmiteshP/nvim-navic",
+    "kosayoda/nvim-lightbulb",
+    {"j-hui/fidget.nvim", tag="legacy", config = function() require("fidget").setup{} end},
+    "folke/neodev.nvim",
+    "folke/todo-comments.nvim",
+    "folke/trouble.nvim",
+
+    -- DAP (Debug Adapter Protocol)
+    "mfussenegger/nvim-dap",
+    "rcarriga/nvim-dap-ui",
+    "theHamsta/nvim-dap-virtual-text",
+    "leoluz/nvim-dap-go",
+
+    -- TS
+    "nvim-treesitter/nvim-treesitter",
+    "norcalli/nvim-colorizer.lua",
+    "nvim-lua/plenary.nvim", -- required by telescope
+    -- git
+    "lewis6991/gitsigns.nvim",
+    "tpope/vim-fugitive",
+    "tpope/vim-rhubarb",
+    "junegunn/gv.vim",
+
+    -- Fuzzy Finding
+    "nvim-telescope/telescope.nvim",
+    "nvim-telescope/telescope-fzf-native.nvim",
+    "nvim-telescope/telescope-ui-select.nvim",
+    -- Notetaking / Wiki
+    "lervag/vimtex",
+    "vimwiki/vimwiki",
+})
--- nvim/lua/statusline.lua
+++ nvim/lua/statusline.lua
@@ -80,18 +80,3 @@ require'nvim-web-devicons'.setup {
   }
  };
 }
-
-require("transparent").setup({
-  extra_groups = { -- table/string: additional groups that should be cleared
-    -- In particular, when you set it to 'all', that means all available groups
-
-    -- example of akinsho/nvim-bufferline.lua
-    "BufferLineTabClose",
-    "BufferlineBufferSelected",
-    "BufferLineFill",
-    "BufferLineBackground",
-    "BufferLineSeparator",
-    "BufferLineIndicatorSelected",
-  },
-  exclude_groups = {}, -- table: groups you don't want to clear
-})
