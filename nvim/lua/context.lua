3535931dab5104e939f9acf50fb0f1cf6ca2e2b8
Jonathan A. <me@iotku.pw>
2024-06-20 23:27:08 -0700

    [nvim] Update gitsigns configuration to new setup, fixes errors at start

--- nvim/lua/context.lua
+++ nvim/lua/context.lua
@@ -76,11 +76,12 @@ require'lsp_signature'.setup{
 vim.opt.signcolumn = "yes:1" -- Always leave space for signcolumn to avoid reflow
 require('gitsigns').setup{
    signs = {
-    add          = {hl = 'GitSignsAdd'   , text = '▐', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
-    change       = {hl = 'GitSignsChange', text = '▐', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-    delete       = {hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-    topdelete    = {hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-    changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+        add          = { text = '┃' },
+        change       = { text = '┃' },
+        delete       = { text = '_' },
+        topdelete    = { text = '‾' },
+        changedelete = { text = '~' },
+        untracked    = { text = '┆' },
   }
 }
 
