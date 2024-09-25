c97e184cc1faa34812aafefee59ee18783a1a3ce
Jonathan A. <me@iotku.pw>
2024-09-25 15:59:17 -0700

    feat: add yank hilight autocmd to nvim from kickstart.nvim

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -26,3 +26,14 @@ vim.api.nvim_create_autocmd({"CursorHold", "CursorHoldI"}, {
         require('nvim-lightbulb').update_lightbulb()
     end,
 })
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
