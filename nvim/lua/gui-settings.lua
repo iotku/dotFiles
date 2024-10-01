85e8d31947fc2138bee421d18bdb3890f9dbc115
Jonathan A. <me@iotku.pw>
2024-09-30 17:28:07 -0700

    fix: set guifont for neovide to a font we actually have

--- nvim/lua/gui-settings.lua
+++ nvim/lua/gui-settings.lua
@@ -1,3 +1,4 @@
 vim.g.neovide_cursor_vfx_mode = "railgun"
 vim.g.neovide_refresh_rate = 60
-vim.o.guifont = "JetBrainsMono Nerd Font:h18"
+vim.g.neovide_scroll_animation_length = 0.1
+vim.o.guifont = "Hack Nerd Font:h14"
