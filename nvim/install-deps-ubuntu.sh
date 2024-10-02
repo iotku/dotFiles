b6302d5da52eb704ec0e5a8a07d1729c58bf40b4
Jonathan A. <me@iotku.pw>
2024-10-02 15:02:21 -0700

    fix: correct shebang for install-deps-ubuntu

--- nvim/install-deps-ubuntu.sh
+++ nvim/install-deps-ubuntu.sh
@@ -1,4 +1,4 @@
-#!/bin/env/bash
+#!/bin/env bash
 echo "Installing neovim and dependencies...."
 sudo apt install neovim python3-venv golang npm rustup openjdk-21-jdk lua5.4
 echo "Installing various golang tools"
