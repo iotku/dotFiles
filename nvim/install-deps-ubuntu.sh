bc556e2be4f4600d7f7c7b52b09c724cd8a99fdb
Jonathan A. <me@iotku.pw>
2024-10-02 20:20:21 -0700

    feat: also install fd-find for ubuntu nvim

--- nvim/install-deps-ubuntu.sh
+++ nvim/install-deps-ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 echo "Installing neovim and dependencies...."
-sudo apt install neovim ripgrep python3-venv golang npm rustup openjdk-21-jdk lua5.4
+sudo apt install neovim ripgrep fd-find python3-venv golang npm rustup openjdk-21-jdk lua5.4
 echo "Installing various golang tools"
 ./install-go-tools.sh
 echo "Launching Neovim"
