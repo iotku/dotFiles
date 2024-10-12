56095ce6e290a1a10e13a4aa25b28c231f15aff8
Jonathan A. <me@iotku.pw>
2024-10-11 17:27:07 -0700

    fix: add unzip dep for ubuntu nvim install

--- nvim/install-deps-ubuntu.sh
+++ nvim/install-deps-ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 echo "Installing neovim and dependencies...."
-sudo apt install curl neovim ripgrep fd-find fzf python3-venv golang npm rustup openjdk-21-jdk lua5.4
+sudo apt install unzip curl neovim ripgrep fd-find fzf python3-venv golang npm rustup openjdk-21-jdk lua5.4
 echo "Installing various golang tools"
 ./install-go-tools.sh
 echo "Launching Neovim"
