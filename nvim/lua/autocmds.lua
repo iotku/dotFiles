1a35927088a8328e14ea6fbaf85a6d48cff463b4
Jonathan A. <me@iotku.pw>
2024-11-10 19:48:47 -0800

    feat(nvim): Run prettier on save on JS/JSX
    We follow vim's tabstop setting

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -20,12 +20,30 @@ vim.api.nvim_create_autocmd("BufEnter", {
     end,
 })
 
--- vim.api.nvim_create_autocmd({"CursorHold", "CursorHoldI"}, {
---     pattern = "*",
---     callback = function()
---         require('nvim-lightbulb').update_lightbulb()
---     end,
--- })
+-- Run prettier on JS/JSX
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = { "*.js", "*.jsx" },
+  group = vim.api.nvim_create_augroup("Prettier", { clear = true }),
+  callback = function()
+    if vim.fn.executable("prettier") == 0 then
+      vim.notify("Prettier is not installed. Please install it with Mason.", vim.log.levels.WARN)
+      return
+    end
+    -- Save cursor position and screen view
+    local cursor_pos = vim.api.nvim_win_get_cursor(0)
+    local view = vim.fn.winsaveview()
+    local filepath = vim.fn.expand("%:p")
+    vim.fn.jobstart({ "prettier", "--write", "--tab-width", tostring(vim.bo.tabstop), filepath }, {
+      on_exit = function()
+        vim.cmd("edit!")  -- Reload the file silently
+        -- Restore cursor position and screen view
+        vim.api.nvim_win_set_cursor(0, cursor_pos)
+        vim.fn.winrestview(view)
+      end,
+    })
+  end,
+})
+
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
